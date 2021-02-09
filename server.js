f4153111c51462814e86c0cc34315c591f0b3e79
Edlyn <edlyn.ksm@gmail.com>
2021-02-09 22:46:39 +0800

    Connected my databse, can see json in the browser

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const app = express();
-
+const food = require('./models/food.js');
 //Port
 
 const port = 5000;
 
 app.get('/oiishidle', (req,res) => {
-    res.send("Welcome to the Oiishi webpage!");
+    res.send(food);
 });
 
 app.listen(port, () => {
